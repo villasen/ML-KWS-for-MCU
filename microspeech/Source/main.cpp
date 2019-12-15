2ab58041f2569d89e590c70d459190c5e6f89498
martin v <villasen@unlv.nevada.edu>
2019-12-14 22:10:01 -0800

    add microspeech

---
+++ microspeech/Source/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 Arm Limited or its affiliates. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Description: Example code for running keyword spotting on Cortex-M boards
+ */
+
+//#include "kws_ds_cnn.h"
+//#include "wav_data.h"
+
+//int16_t audio_buffer[16000]=WAVE_DATA;
+
+//Timer T;
+//Serial pc(USBTX, USBRX);
+
+int main()
+{
+  int test=0;
+
+  return 0;
+}
